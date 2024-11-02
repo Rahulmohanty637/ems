530c49130f25f5aa5855dc5677409683e5759b3d
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-11-02 23:54:19 +0530

    Update localStorage.jsx

---
+++ src/utils/localStorage.jsx
@@ -0,0 +1,35 @@
+const employees = [
+  {
+    id: "E001",
+    email: "employee1@example.com",
+    password: "123",
+  },
+  {
+    id: "E002",
+    email: "employee2@example.com",
+    password: "123",
+  },
+  {
+    id: "E003",
+    email: "employee3@example.com",
+    password: "123",
+  },
+  {
+    id: "E004",
+    email: "employee4@example.com",
+    password: "123",
+  },
+  {
+    id: "E005",
+    email: "employee5@example.com",
+    password: "123",
+  },
+];
+
+const admin = [
+  {
+    id: "A001",
+    email: "admin@example.com",
+    password: "123",
+  },
+];
