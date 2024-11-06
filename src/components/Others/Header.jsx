e516dc9561f8e56d9578384d8ad3d008d974d6db
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-11-06 23:33:41 +0530

    Task List updated

--- src/components/Others/Header.jsx
+++ src/components/Others/Header.jsx
@@ -5,7 +5,7 @@ const Header = ({ data }) => {
     <div className="flex items-end justify-between">
       <h1 className="text-xl font-medium">
         Hello <br />
-        <span className="text-3xl font-semibold"> Rahul 🖐️</span>
+        <span className="text-3xl font-semibold"> {data.firstName} 🖐️</span>
       </h1>
       <button className="bg-red-600 text-lg font-medium px-5 py-2 rounded-md hover:bg-red-800">
         Log Out
--- src/components/TaskList/NewTask.jsx
+++ src/components/TaskList/NewTask.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const NewTask = () => {
   return (
-    <div>
-      
+    <div className="flex-shrink-0 bg-green-400 h-full w-[300px] p-5 rounded-xl">
+      <div className="flex justify-between items-center">
+        <h3 className="bg-red-600 text-sm px-3 py-1 rounded-md">High</h3>
+        <h4 className="text-sm">20 Feb 2024</h4>
+      </div>
+      <h2 className="mt-5 text-2xl font-semibold">Complete This Project</h2>
+      <p className="text-sm mt-2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut, facilis
+        atque! Deleniti aut voluptate nihil?
+      </p>
+      <div className="mt-4">
+        <button>Accept Task</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewTask
+export default NewTask;
