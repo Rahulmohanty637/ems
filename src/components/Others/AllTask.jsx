3ed48d9e884e5489a790b28ac354dded08c213a0
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-11-09 00:52:13 +0530

    Update AllTask.jsx

--- src/components/Others/AllTask.jsx
+++ src/components/Others/AllTask.jsx
@@ -5,16 +5,35 @@ const AllTask = () => {
   const authData = useContext(AuthContext);
 
   return (
-    <div className="bg-[#1c1c1c] p-5 rounded mt-5 h-48 overflow-auto">
-      {authData.employees.map(function (element) {
-        return (
-          <div className="bg-red-400 mb-2 py-2 px-4 flex justify-between rounded">
-            <h3>{element.firstName}</h3>
-            <h3>Makea a UI Design</h3>
-            <h5>Status</h5>
-          </div>
-        );
-      })}
+    <div className="bg-[#1c1c1c] p-5 rounded mt-5 h-48">
+      <div className="bg-red-400 mb-2 py-2 px-4 flex justify-between rounded">
+        <h3 className="w-1/5">Employee Name</h3>
+        <h3 className="w-1/5">New Task</h3>
+        <h5 className="w-1/5">Active Task</h5>
+        <h5 className="w-1/5">Completed</h5>
+        <h5 className="w-1/5">Failed</h5>
+      </div>
+      <div className="h-full">
+        {authData.employees.map(function (element) {
+          return (
+            <div className="mb-2 py-2 px-4 flex justify-between rounded">
+              <h3 className="text-lg font-medium w-1/5">{element.firstName}</h3>
+              <h5 className="text-lg font-medium w-1/5 text-blue-600">
+                {element.taskNumbers.newTask}
+              </h5>
+              <h3 className="text-lg font-medium w-1/5 text-yellow-600">
+                {element.taskNumbers.active}
+              </h3>
+              <h5 className="text-lg font-medium w-1/5 text-green-600">
+                {element.taskNumbers.completed}
+              </h5>
+              <h5 className="text-lg font-medium w-1/5 text-red-600">
+                {element.taskNumbers.failed}
+              </h5>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
